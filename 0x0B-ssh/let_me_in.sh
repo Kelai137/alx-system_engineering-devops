8f1c6b10b48e417fe80b906fc26f70ec4251b775
Kelai137 <moseskelaisakala137@gmail.com>
2023-06-25 22:48:43 -0700

    UPDATE

--- 0x0B-ssh/let_me_in.sh
+++ 0x0B-ssh/let_me_in.sh
@@ -5,7 +5,7 @@ server=107.22.144.99
 user="ubuntu"
 
 # SSH public key
-ssh_public_key="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDYYWE6i4Zfaazl9mhKLXCZnthW+d4EKldrNHJhuMwIqLXj3Ff2Y3TI91p+vuCwI3ue23sd0uiUOVe4MkXZyUVc5hVeLlVC41AtxS0Qsv+xXPodq8aqJvlp7CVizJiUOsui9JtRqrTrWQqi6JtX8V3vW4QyOseivH1c6Plykcbqw+QcKESnZ7IAXnzVaR3d1eNUubxA3OXQijDwW2grCle3GEgPsOPnQ967VkhG3xS31gQc+XXLYywnkMALPguvZ0AoZP7dZtDXDMYk2dRTNrakR8E4H+5JGEW95PD54C2u/2Im8YkzR2q03xlhVCcZZ71P3P/sF52BahtkMUDC2lJdfb8OydNqFCAAIVPIX2MjrHEUN7fuzKPJlE6VxPNGWGnDM8yz7HryJzBTHXc9qMPWrBFO2VH+EEkpECpeEhqi+DIZb3Cin4YFIl0MnUHWF8y0jg0EUHyR3ZK9u/NXNR8xYe4L9SvECAwGGlBZZ85/6jzqlpBLfwdvFJodfgAkPFk= moses sakala@DESKTOP-LHDTL0J"
+ssh_public_key="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDNdtrNGtTXe5Tp1EJQop8mOSAuRGLjJ6DW4PqX4wId/Kawz35ESampIqHSOTJmbQ8UlxdJuk0gAXKk3Ncle4safGYqM/VeDK3LN5iAJxf4kcaxNtS3eVxWBE5iF3FbIjOqwxw5Lf5sRa5yXxA8HfWidhbIG5TqKL922hPgsCGABIrXRlfZYeC0FEuPWdr6smOElSVvIXthRWp9cr685KdCI+COxlj1RdVsvIo+zunmLACF9PYdjB2s96Fn0ocD3c5SGLvDOFCyvDojSAOyE70ebIElnskKsDTGwfT4P6jh9OBzTyQEIS2jOaE5RQq4IB4DsMhvbjDSQrP0MdCLgwkN"
 
 # Connect to the server and add the SSH public key to authorized_keys
 ssh ubuntu@107.22.144.99 "mkdir -p ~/.ssh && echo \"$ssh_public_key\" >> ~/.ssh/authorized_keys && chmod 700 ~/.ssh && chmod 600 ~/.ssh/authorized_keys"
